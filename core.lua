c945785ebdff084885840f3f9288e641d2910db0
Martin Jesper Low Madsen <martin_jlowm@hotmail.com>
2010-03-02 15:59:16 +0100

    asd'

--- core.lua
+++ core.lua
@@ -38,7 +38,7 @@ local Update = function(self, event, destUnit, endTime)
 		elseif not unitName then
 			unitName = destUnit
 		end
-		
+		 
 		local beingRessed, resserName = libResComm:IsUnitBeingRessed(unitName)
 		if beingRessed and not (resComm.OthersOnly and resserName == playerName) then
 			if resComm:IsObjectType("Statusbar") then
